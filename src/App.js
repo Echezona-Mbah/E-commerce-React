819ee8de3698b4599ad6d286d546801f0822735e
Echezona-Mbah <cclassico998@gmail.com>
2024-08-27 10:48:20 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useParams, Link } from 'react-router-dom';
 import './App.css';
 import HomePage from './page/homepage/homepage.component';
 
-const HatsPage = (props) =>{
-  console.log(props)
-  return <h1>Hats Page</h1>
+const HatsPage = (props) => {
+  console.log(props);
+  return <h1>Hats Page</h1>;
 };
 
-const TopicList = () =>{
-  return <h1>Topic List Page</h1>
+const TopicList = () => {
+  return <h1>Topic List Page</h1>;
 };
 
-const TopicDeteil = (props) =>{
-  console.log(props)
-  return  <h1>Topic Detail Page{props.match.params.topicId}</h1>
+const TopicDetail = () => {
+  const { topicId } = useParams(); // Get the route parameter
+
+  return (
+    <div>
+      <Link to="/hats">Go to Hats Page</Link>
+      <h1>Topic Detail Page: {topicId}</h1>
+    </div>
+  );
 };
 
 function App() {
@@ -24,7 +30,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/hats" element={<HatsPage />} />
         <Route path="/topiclist" element={<TopicList />} />
-        <Route path="/topicdetail/:topicId" element={<TopicDeteil />} />
+        <Route path="/topicdetail/:topicId" element={<TopicDetail />} />
       </Routes>
     </Router>
   );
