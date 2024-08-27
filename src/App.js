548dda5e6653fb1c92aa27acb4a1ae3629af485a
Echezona-Mbah <cclassico998@gmail.com>
2024-08-26 22:18:40 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 import HomePage from './page/homepage/homepage.component';
 
-const HatsPage = () => <h1>Hats Page</h1>;
+const HatsPage = () =>{
+  <h1>Hats Page</h1>
+};
+
+const TopicList = () =>{
+  <h1>Topic List Page</h1>
+};
+
+const TopicDeteil = () =>{
+  <h1>Topic Detail Page</h1>
+};
 
 function App() {
   return (
-    <div>
-          <Route path="/" component={HomePage} />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/hats" element={<HatsPage />} />
+        <Route path="/topiclist" element={<TopicList />} />
+        <Route path="/topicdetail/:topicId" element={<TopicDeteil />} />
+      </Routes>
+    </Router>
   );
 }
-
 
 export default App;
