142dbd7a99122d9ba0f9b3bb9582c92249d7a2af
Echezona-Mbah <cclassico998@gmail.com>
2024-08-28 04:59:10 -0700

    first commit

--- src/component/manu-item/manu-item.component.jsx
+++ src/component/manu-item/manu-item.component.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import './manu-item.style.css';
 
-
-const ManuItem = ({title}) => {
-    <div className="menu-item">
-    <div className="content">
-      <h1 className="title">{title}</h1>
-      <span className="subtitle">SHOP NOW</span>
-    </div>
-  </div>
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+    return (
+        <div className={`${size} menu-item`}>
+            <div
+                className="background-image"
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
+            <div className="content">
+                <h1 className="title">{title.toUpperCase()}</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+    );
 }
 
-export default ManuItem;
+export default MenuItem;
--- src/page/homepage/homepage.component.jsx
+++ src/page/homepage/homepage.component.jsx
@@ -1,44 +1,13 @@
 import React from "react";
-import './homepage.style.css'
+import Directory from "../../component/directory/directory.component";
+import './homepage.style.css';
 
 const HomePage = () => {
   return (
     <div className="homepage">
-      <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">JACKETS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">SNEAKERS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">WOMAN</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">MEN</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-      </div>
+      <Directory />
     </div>
   );
 };
 
-// Exporting the HomePage component
 export default HomePage;
