3f3e12d35e3fdeb67f07f3558c7e58086ac6053e
Echezona-Mbah <cclassico998@gmail.com>
2024-09-02 15:38:06 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
-        {/* <Route path="/topicdetail/:topicId" element={<TopicDetail />} /> */}
+        {/* <Route path="/topicdetail/:topicId" element={<TopicDetail />} /> jjjjjjjjjjjj*/}
       </Routes>
     </Router>
   );
