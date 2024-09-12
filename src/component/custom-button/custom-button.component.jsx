e161560a681f6c0e29a8c5be8d84f11dcc8380f0
Echezona-Mbah <cclassico998@gmail.com>
2024-09-12 09:23:32 -0700

    first commit

--- src/component/custom-button/custom-button.component.jsx
+++ src/component/custom-button/custom-button.component.jsx
@@ -4,7 +4,7 @@ import './custom-button.style.css';
 
 const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
   <button
-    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}  // Addgdce between classes
+    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}  // Adween classes
     {...otherProps}
   >
     {children}
