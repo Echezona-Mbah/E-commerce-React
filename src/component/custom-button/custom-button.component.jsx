2ba837504623f48461e32004b91d73d196e0d0f1
Echezona-Mbah <cclassico998@gmail.com>
2024-11-07 22:05:45 -0800

    Add .gitattributes for Git LFS

--- src/component/custom-button/custom-button.component.jsx
+++ src/component/custom-button/custom-button.component.jsx
@@ -4,7 +4,7 @@ import './custom-button.style.css';
 
 const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
   <button
-    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}  //jgyyp0p0ddcghjkhukhkuclasses
+    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}  //jgyypddcghjkhukhkuclasses
     {...otherProps}
   >
     {children}
